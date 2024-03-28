0e7fd82168342e4aa1422edab9f10b37bb0fc845
Joseph Noblett <noblettjoe@gmail.com>
2024-03-28 14:02:24 -0400

    DS: Arrays: reverse strings

--- Arrays/reverse.js
+++ Arrays/reverse.js
@@ -1,7 +1,21 @@
+const reverseFunctionally = str => str.toLowerCase().split('').reverse().join('');
 
+const reverseFunctionallySpead = str => [...str.toLowerCase()].reverse().join('');
 
-function reverse(str) {
-  return str.toLowerCase().split('').reverse().join('');
+function reverseLoops(str) {
+ if (!str || str.length < 2 ) {
+    return "str error";
+  } 
+  const backwards = [];
+  const items_length = str.length - 1;
+
+  for (let i = items_length; i >= 0; i--) {
+    backwards.push(str[i].toLowerCase())
+  }
+
+  return backwards.join('');
 }
 
-console.log(reverse('Noblett'))
+console.log(reverseFunctionally('Noblett'));
+console.log(reverseFunctionallySpead('Noblett'))
+console.log(reverseLoops('Noblett'));
