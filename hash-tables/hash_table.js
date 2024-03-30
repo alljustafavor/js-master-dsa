c15877fcd6f981a5292ed93c9abb24e71c92580a
Joseph Noblett <noblettjoe@gmail.com>
2024-03-30 11:06:48 -0400

    DS: Hash Tables added values method

--- hash-tables/hash_table.js
+++ hash-tables/hash_table.js
@@ -33,21 +33,47 @@ class HashTable {
     return undefined;
   } // Collision: O(n) | !Collision: O(1)
 
-  keys() { // No order
-    let addressArray = [];
+   keys() {
+    if (!this.data.length) return undefined
+    let result = []
     for (let i = 0; i < this.data.length; i++) {
-      if (this.data[i]) {
-        addressArray.push(this.data[i][0][0])
+      if (this.data[i] && this.data[i].length) {
+        if (this.data.length > 1) {
+          for (let j = 0; j < this.data[i].length; j++) {
+            result.push(this.data[i][j][0])
+          }
+        } else {
+          result.push(this.data[i][0])
+        } 
       }
     }
-    return addressArray;
+    return result; 
+  }
+
+  values() {
+    if (!this.data.length) return undefined;
+    let valuesArray = [];
+
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i] && this.data.length) {
+        if (this.data.length > 1) {
+          for (let j = 0; j < this.data[i].length; j++) {
+            valuesArray.push(this.data[i][j][1]);
+          }
+        } else {
+            valuesArray.push(this.data[i][1]) 
+        }
+      }
+    }
+    return valuesArray;
   }
 }
 
 const myHashTable = new HashTable(50);
 myHashTable.set("grapes", 10000);
 myHashTable.set("apples", 45);
-myHashTable.set('pairs', 10);
+myHashTable.set("pairs", 10);
 myHashTable.get("grapes");
+console.log(myHashTable);
 console.log(myHashTable.keys());
-
+console.log(myHashTable.values());
